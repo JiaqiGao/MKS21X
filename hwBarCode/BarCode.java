d275754092ba613946c2f6082b2c8e073b0ca460
JiaqiGao <jiaqigao12@gmail.com>
2015-12-28 16:22:50 -0500

    Added checkSum

--- hwBarCode/BarCode.java
+++ hwBarCode/BarCode.java
@@ -1,8 +1,7 @@
-public class BarCode{
-//implements Comparable{
+public class BarCode implements Comparable{
     private String _zip;
     private int _checkDigit;
-    public static String[] key = new String[] {"||:::",":::||","::|:|","::||:",":|::|",":|:|:",":||::","|:::|","|::|:","|:|::"};
+    public static String[] key = new String[]{"||:::",":::||","::|:|","::||:",":|::|",":|:|:",":||::","|:::|","|::|:","|:|::"};
 
     
     // constructors
@@ -10,56 +9,29 @@ public class BarCode{
     //postcondition: throws a runtime exception zip is not the correct length
     //               or zip contains a non digit
     //               _zip and _checkDigit are initialized.
-    public BarCode(String zip) {
-        _zip = zip;
-        _checkDigit = checkSum();
-    }
+    public BarCode(String zip) {}
 
     // postcondition: Creates a copy of a bar code.
-    // public BarCode(BarCode x){
-        
-    // }
+    public BarCode(BarCode x){}
 
 
     //post: computes and returns the check sum for _zip
-    private int checkSum(){
-        int sum = 0;
-        for (int i=0; i<5; i++){
-            sum+=Integer.parseInt(_zip.substring(i,i+1));
-        }
-        sum = sum%10;
-        return sum;
-    }
+    private int checkSum(){}
 
     //postcondition: format zip + check digit + barcode 
     //ex. "084518  |||:::|::|::|::|:|:|::::|||::|:|"      
-    public String toString(){
-        String barcode = _zip + _checkDigit + " |";
-        for (int i=0; i<5; i++){
-            barcode+=key[Integer.parseInt(_zip.substring(i,i+1))];
-        }  
-        barcode+= key[_checkDigit] + "|";
-        return barcode; 
-    }
-    
-    public static void main(String[]arggs){
-        BarCode bc = new BarCode("08451");
-        System.out.println(bc);
-    }
+    public String toString(){}
 
 
-    // public boolean equals(Object other){}
+    public boolean equals(Object other){}
     // postcondition: false if the object is not a BarCode, 
     // false if it is a non-matching barcode
     // true when they match.
 
 
-    public int compareTo(Comparable other){
-        return 1;
-    }
-    // postcondition: compares the zip + checkdigit
+    public int compareTo(Comparable other){}
+    // postcondition: compares the zip + checkdigit 
+
+
+
 }
-
-
-
-
